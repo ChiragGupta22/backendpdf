a41a858ab6c5048be5a521e3a3a5c677523f2e4f
Chirag <chiragguptaworks@gmail.com>
2026-06-13 22:22:36 +0530

    backend + frontend

--- main.py
+++ main.py
@@ -14,8 +14,7 @@ def root():
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "https://frontendpdf.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
